eefae449a1f815ce9800a41360658514121fca21
FreddieOffice <70265740+FreddieOffice@users.noreply.github.com>
2023-01-14 02:52:22 +0300

    Update 1dc.lua

--- 1dc.lua
+++ 1dc.lua
@@ -1,6 +1,6 @@
 local vars = {}
 
-local print = print
+local print, error = print, error
 local line_count = 1
 
 local function wait(s) 
@@ -15,7 +15,7 @@ function main()
 
         line_count = line_count + 1
 
-        for printw, word in line:gmatch("(.-) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+)") do
+        for printw, word in line:gmatch("(.-) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+)") do
             if printw == "p" then
                 print(word.."\n")
             elseif printw == "pwn" then
@@ -27,33 +27,44 @@ function main()
             end
         end
 
-        for key, value in line:gmatch("v ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+) = ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+)") do
-            vars[key] = value
+        for vartype, key, value in line:gmatch("(.-) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+) = ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+)") do
+            if vartype == "int" then
+                vars[key] = tonumber(value) or error("NOT AN NUMBER")
+            end
+            if vartype == "string" then
+                vars[key] = tostring(value)
+            end
         end
 
 
-        for func, param in line:gmatch("(.-) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+)") do
+        for func, param in line:gmatch("(.-) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+)") do
             if func == "r" then
                 local r = io.read()
                 vars[param] = r
             elseif func == "w" then
-                wait(param/2)
+                wait(param/2+param/2)
             elseif func == "exec" then
                 os.execute(param)
             end
         end
 
-        for filename, var in line:gmatch("o ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+) ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%p%s%z]+)") do
-            local f = io.open(filename, "r")
+        for filename_r, var in line:gmatch('or ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+) = ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+)') do
+            local f = io.open(filename_r, "r")
             local data = f:read("*all")
             f:close()
 
             vars[var] = data
+        end
+
+        for filename_w, towrite in line:gmatch('ow ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+) = ([abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%d%p%s%z]+)') do
+            local f = io.open(filename_w, "w")
+            f:write(towrite)
+            f:close()
         end
     end
 end
 
 local ok,err = pcall(main)
 if not ok then
-    print("ONE-C ERROR "..err)
+    print("1DC ERROR "..err)
 end
